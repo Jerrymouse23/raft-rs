b101d6da267ecbd8f238c331b8883a7003acf5c1
kper <kevp.per@gmail.com>
2017-03-14 09:28:36 +0100

    Implement Sha256 auth

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -3,11 +3,12 @@ use std::fmt::Debug;
 pub mod null;
 pub mod credentials;
 pub mod simple;
+pub mod sha256;
 
 /// A trait to do authentification
 pub trait Auth: Clone + Debug + Send + 'static {
     /// Generates hash of type T
-    fn generate(plain: &str) -> String;
+    fn hash(&self,plain: &str) -> String;
 
     /// Checks hash and returns whether it was successful or not
     fn compare(&self, plain: &str, hash: &str) -> bool;
--- src/auth/sha256.rs
+++ src/auth/sha256.rs
@@ -2,26 +2,31 @@
 use auth::Auth;
 use auth::credentials::Credentials;
 
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
+
 #[derive(Debug,Clone)]
-pub struct SimpleAuth<C>
+pub struct Sha256Auth<C>
     where C: Credentials
 {
     credentials: C,
 }
 
-impl<C> SimpleAuth<C>
+impl<C> Sha256Auth<C>
     where C: Credentials
 {
     pub fn new(credentials: C) -> Self {
-        SimpleAuth { credentials: credentials }
+        Sha256Auth { credentials: credentials }
     }
 }
 
-impl<C> Auth for SimpleAuth<C>
+impl<C> Auth for Sha256Auth<C>
     where C: Credentials
 {
     fn hash(&self,plain: &str) -> String {
-        plain.to_string()
+        let mut hasher = Sha256::new();
+        hasher.input_str(plain);
+        hasher.result_str()
     }
 
     fn compare(&self, hash1: &str, hash2: &str) -> bool {
